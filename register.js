6888f05684ab674ce93a7b62f2f58e49f6e0675d
RoghreSoares <rogher_adriano@hotmail.com>
2025-05-27 21:30:07 -0300

    verificando se o cep e valido e tem 8 digitos

--- register.js
+++ register.js
@@ -3,4 +3,4 @@
 // Verifica se o CEP é válido
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
 // Verifica se o CEP tem 8 dígitos
-const cepValido = (cep) => cep.length === 8 && eNumero(cep);
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
