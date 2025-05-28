c1ee923a3dcb0090d663d25c1e5cc3ac70083126
RoghreSoares <rogher_adriano@hotmail.com>
2025-05-27 21:54:17 -0300

    logica par validacao do CEP e preenchimento do mesmo e limapndo o formulario

--- register.js
+++ register.js
@@ -4,3 +4,28 @@
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
 // Verifica se o CEP tem 8 dígitos
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+const pesquisarCep = async () => {
+    limparFormulario();
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+
+    if (cepValido(cep.value)) {
+        const dados = await fetch(url);
+        const address = await dados.json();
+
+        //hasOwnProperty verifica se o objeto tem a propriedade especificada e retorna true ou false
+        if (address.hasOwnProperty('erro')) {
+            alert('CEP não encontrado!');
+        } else {
+            preencherFormulario(address);
+        }
+    }
+}
+
+//Função para limpar o formulario
+const limparFormulario = () => {
+    document.getElementById('rua').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
