20e97f9633c6d140e35c51a635488ddd69498ee0
Rogher Adriano Soares <163779899+Roogher@users.noreply.github.com>
2025-06-04 01:07:14 -0300

    codigo funcionando

--- register.js
+++ register.js
@@ -1,44 +1,47 @@
-'use strict'; //modo restrito
+'use strict'; //Modo "Restrito"
+//Consumindo API de CEP, do ViaCep
+// https://viacep.com.br/
 
-// Verifica se o CEP é válido
-const eNumero = (numero) => /^[0-9]+$/.test(numero);
-// Verifica se o CEP tem 8 dígitos
+//Verifica se o CEP é válido...
+const eNumero = (numero) => /^[0-9]+$/.test(numero); //Expressão Regular
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 
-const pesquisarCep = async () => {
+//Consumindo API... 2- passo
+const pesquisarCep = async() => {
     limparFormulario();
     const url = `http://viacep.com.br/ws/${cep.value}/json/`;
-
-    if (cepValido(cep.value)) {
-        const dados = await fetch(url);
-        const addres = await dados.json();
-
-        //hasOwnProperty verifica se o objeto tem a propriedade especificada e retorna true ou false
-        if (addres.hasOwnProperty('erro')) {
+    if(cepValido(cep.value)){
+        const dados = await fetch(url); //await = esperar fetch = promessa
+        const addres = await dados.json(); 
+        
+        // hasOwnProperty  retorna um booleano indicando se o objeto possui a propriedade especificada como uma propriedade definida no próprio objeto em questão
+        if(addres.hasOwnProperty('erro')){ 
+            // document.getElementById('rua').value = 'CEP não encontrado!';
             alert('CEP não encontrado!');
-        } else {
-            preencherFormulario(addres);
+        }else {
+            preencherForumulario(addres);
         }
-    }
+    }else{
+        // document.getElementById('rua').value = 'CEP incorreto!';
+        alert('CEP incorreto!');
+    } 
 }
 
-//Função para preencher o formulario
-const preencherFormulario = (endereco) => {
-    document.getElementById('rua').value = endereco.logradouro;
-    document.getElementById('bairro').value = endereco.bairro;
-    document.getElementById('cidade').value = endereco.localidade;
-    document.getElementById('estado').value = endereco.estado;
-}
-
-//Função para limpar o formulario
-const limparFormulario = () => {
+//Limpa o Form (do CEP pra baixo)...
+const limparFormulario = () =>{
     document.getElementById('rua').value = '';
     document.getElementById('bairro').value = '';
     document.getElementById('cidade').value = '';
     document.getElementById('estado').value = '';
-    document.getElementById('cep').value = '';
-    document.getElementById('numero').value = '';
-    document.getElementById('complemento').value = '';
 }
 
-document.getElementById("cep").addEventListener("focusout", pesquisarCep);
+//Preenche os campos relacionados ao CEP...
+const preencherForumulario = (endereco) =>{
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+//Adicionando um evento DOM, no input CEP... 1- passo
+document.getElementById('cep').addEventListener('focusout', pesquisarCep);
